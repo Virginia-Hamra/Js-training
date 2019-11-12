c881a7b97124d8d07de5ee5411c191fc5cd1a714
Virginia-Hamra <virhamra@outlook.com>
2019-11-12 01:35:09 -0300

    Create for-nested.js

---
+++ for-nested.js
@@ -0,0 +1,19 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<script type="text/javascript">
+		
+		for (i=0; i<5; i++) {
+			console.log(i);
+				for(z=0; z<1; z++){
+					console.log(z)
+				}
+		}
+	</script>
+	
+</body>
+</html>
